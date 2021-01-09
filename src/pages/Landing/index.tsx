4b2fff6c4486ca20c228f9ce09ca779d45679ce2
Martynas Stumbra <50275578+marstu2@users.noreply.github.com>
2021-01-09 19:16:43 +0200

    feat: adjusted landing page and added api key for google apis

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,45 +1,80 @@
-import { Fragment, useState, MouseEvent, ChangeEvent } from 'react';
+import { Fragment, useState, MouseEvent } from 'react';
 import * as S from './styles';
-import { Header, Footer, Toggler } from '../../components';
+import { Header, Footer, Button, Table } from '../../components';
 import csc from 'country-state-city';
-import { CountryType } from '../../types';
+import { CityType, CountryType } from '../../types';
 
 const Landing: React.FC = () => {
   const [currentSection, setCurrentSection] = useState<number>(0);
   const [searchInputValue, setSearchInputValue] = useState<string>('');
-  const [selectedCountry, setSelectedCountry] = useState<CountryType>();
 
-  const countries = csc.getAllCountries().filter((country) => {
-    return country.name.toLowerCase().includes(searchInputValue.toLowerCase());
-  });
+  const isChooseByCountrySection = currentSection === 0;
+
+  const [selectedValue, setSelectedValue] = useState<any>({});
+
+  const countries = isChooseByCountrySection
+    ? csc.getAllCountries().filter((country) => {
+        return country.name.toLowerCase().includes(searchInputValue.toLowerCase());
+      })
+    : [];
 
   const handleSectionClick = (event: MouseEvent<HTMLButtonElement>) => {
+    setSelectedValue({});
     setCurrentSection(Number(event.currentTarget.id));
+    setSearchInputValue('');
   };
 
-  const handleSearchInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setSearchInputValue(event.currentTarget.value);
+  const handleSearchInputChange = (value: string) => {
+    setSearchInputValue(value);
   };
 
   const handleOnCountryPress = (country: CountryType) => {
-    setSelectedCountry(country);
+    setSelectedValue(country);
     setSearchInputValue(country.name);
+  };
+
+  const handleOnCityPress = (city: CityType[]) => {
+    setSelectedValue(city[0]);
+    setSearchInputValue(city[0].formatted_address);
   };
 
   return (
     <Fragment>
       <Header />
       <S.Container>
-        <Toggler
-          onSectionClick={handleSectionClick}
-          currentSection={currentSection}
-          buttonTitles={{ primary: 'Choose by Country', secondary: 'Search by City' }}
-          onSearchInputChange={handleSearchInputChange}
-          searchInputValue={searchInputValue}
-          countries={countries}
-          onCountryPress={handleOnCountryPress}
-          selectedCountry={selectedCountry}
-        />
+        <S.ButtonWrapper>
+          <Button
+            id="0"
+            title="Choose by country"
+            active={isChooseByCountrySection}
+            onClick={handleSectionClick}
+          />
+          <Button
+            id="1"
+            title="Search by city"
+            active={!isChooseByCountrySection}
+            onClick={handleSectionClick}
+          />
+        </S.ButtonWrapper>
+        {Object.keys(selectedValue).length === 0 ? (
+          <S.TableWrapper>
+            <Table
+              data={isChooseByCountrySection ? countries : undefined}
+              title={isChooseByCountrySection ? 'Countries' : 'Cities'}
+              onCellClick={isChooseByCountrySection ? handleOnCountryPress : handleOnCityPress}
+              searchInputValue={searchInputValue}
+              onSearchInputChange={handleSearchInputChange}
+              selectedValue={selectedValue}
+              withGoogleAutoComplete={!isChooseByCountrySection}
+            />
+          </S.TableWrapper>
+        ) : (
+          <h3>{`You have selected ${
+            selectedValue?.name
+              ? `${selectedValue.name} as a country of interest.`
+              : `${selectedValue.formatted_address} as a city of interest.`
+          }`}</h3>
+        )}
       </S.Container>
       <Footer />
     </Fragment>
--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -2,6 +2,25 @@ import styled from 'styled-components';
 
 const Container = styled.main`
   flex: 1;
+  h3 {
+    color: black;
+
+    text-align: center;
+  }
 `;
 
-export { Container };
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 0.75rem;
+  button {
+    margin: 0.3rem;
+  }
+`;
+
+const TableWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export { Container, ButtonWrapper, TableWrapper };
