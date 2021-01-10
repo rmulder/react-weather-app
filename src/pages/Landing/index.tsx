036fe49570ad0993e33520f78380c13ab50ee953
Martynas Stumbra <50275578+marstu2@users.noreply.github.com>
2021-01-10 18:47:40 +0200

    feat: added google maps window w/ weather details
    Also added favorite cities add/remove functionality from localStorage

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,42 +1,147 @@
-import { Fragment, useState, MouseEvent } from 'react';
+import { Fragment, useState, MouseEvent, useEffect } from 'react';
 import * as S from './styles';
-import { Header, Footer, Button, Table } from '../../components';
+import { Header, Footer, Button, Table, Popup } from '../../components';
 import csc from 'country-state-city';
-import { CityType, CountryType } from '../../types';
+import { CountryType, CityType, WeatherType } from '../../types';
+import { isObjectEmpty } from '../../utils/helpers';
+import { GoogleMap, InfoWindow } from '@react-google-maps/api';
+import { getWeatherByCoordinates } from '../../api/weather';
+
+const mapContainerStyle = {
+  width: '70vw',
+  height: '562px',
+};
+
+// Default Kaunas City coordinates
+const defaultCoordinates = {
+  latitude: 54.8985,
+  longitude: 23.9036,
+};
 
 const Landing: React.FC = () => {
   const [currentSection, setCurrentSection] = useState<number>(0);
-  const [searchInputValue, setSearchInputValue] = useState<string>('');
+
+  const [primaryInputValue, setPrimaryInputValue] = useState<string>('');
+  const [secondaryInputValue, setSecondaryInputValue] = useState<string>('');
 
   const isChooseByCountrySection = currentSection === 0;
 
-  const [selectedValue, setSelectedValue] = useState<any>({});
+  const [selectedCountry, setSelectedCountry] = useState<CountryType>({} as CountryType);
+  const [selectedCity, setSelectedCity] = useState<CityType>({} as CityType);
+
+  const [weather, setWeather] = useState<WeatherType>({} as WeatherType);
+
+  const [deviceLocation, setDeviceLocation] = useState<{ latitude: number; longitude: number }>(
+    defaultCoordinates
+  );
+
+  const [favoriteCities, setFavoriteCities] = useState<any[]>(
+    JSON.parse(localStorage.getItem('fav-cities') || '[]')
+  );
+
+  const handleDeviceGeolocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        setDeviceLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+        setWeather(
+          await getWeatherByCoordinates(position.coords.latitude, position.coords.longitude)
+        );
+      },
+      () => alert('Geolocation is not supported by this browser.'),
+      { enableHighAccuracy: true, maximumAge: 1000 }
+    );
+  };
+
+  useEffect(() => {
+    if (deviceLocation === defaultCoordinates) handleDeviceGeolocation();
+  }, []);
 
   const countries = isChooseByCountrySection
     ? csc.getAllCountries().filter((country) => {
-        return country.name.toLowerCase().includes(searchInputValue.toLowerCase());
+        return country.name.toLowerCase().includes(primaryInputValue.toLowerCase());
       })
     : [];
 
-  const handleSectionClick = (event: MouseEvent<HTMLButtonElement>) => {
-    setSelectedValue({});
-    setCurrentSection(Number(event.currentTarget.id));
-    setSearchInputValue('');
+  const cities =
+    isChooseByCountrySection && selectedCountry?.name
+      ? csc.getCitiesOfCountry(selectedCountry.isoCode).filter((city) => {
+          return city.name.toLowerCase().includes(secondaryInputValue.toLowerCase());
+        })
+      : !isChooseByCountrySection
+      ? csc.getAllCities().filter((city) => {
+          return city.name.toLowerCase().includes(primaryInputValue.toLowerCase());
+        })
+      : [];
+
+  const handleOnCellClick = async (value: any, type: string) => {
+    if (type === 'country') {
+      setWeather({} as WeatherType);
+      setSelectedCountry(value);
+      setPrimaryInputValue(value.name);
+    } else if (!isChooseByCountrySection) {
+      setWeather(await getWeatherByCoordinates(value.latitude, value.longitude));
+      setSelectedCity(value);
+      setPrimaryInputValue(value.name);
+    } else {
+      setWeather(await getWeatherByCoordinates(value.latitude, value.longitude));
+      setSelectedCity(value);
+      setSecondaryInputValue(value.name);
+    }
   };
 
-  const handleSearchInputChange = (value: string) => {
-    setSearchInputValue(value);
+  const handleSectionClick = async (event: MouseEvent<HTMLButtonElement>) => {
+    setPrimaryInputValue('');
+    setSecondaryInputValue('');
+    setCurrentSection(+event.currentTarget.id);
   };
 
-  const handleOnCountryPress = (country: CountryType) => {
-    setSelectedValue(country);
-    setSearchInputValue(country.name);
+  const handleOnGeolocationClick = () => {
+    setSelectedCity({} as CityType);
+    setSelectedCountry({} as CountryType);
+    setPrimaryInputValue('');
+    setSecondaryInputValue('');
+    handleDeviceGeolocation();
   };
 
-  const handleOnCityPress = (city: CityType[]) => {
-    setSelectedValue(city[0]);
-    setSearchInputValue(city[0].formatted_address);
+  const handleAddRemoveCityFromFavorites = () => {
+    const favoriteCitiesCopy = [...favoriteCities];
+
+    const index = favoriteCitiesCopy.findIndex((city: any) => {
+      return city.name === weather.name;
+    });
+
+    if (index < 0) {
+      const newCity = {
+        name: weather.name,
+        coordinates: { latitude: weather.coord.lat, longitude: weather.coord.lon },
+      };
+      favoriteCitiesCopy.push(newCity);
+    } else favoriteCitiesCopy.splice(index, 1);
+
+    setFavoriteCities(favoriteCitiesCopy);
+    localStorage.setItem('fav-cities', JSON.stringify(favoriteCitiesCopy));
   };
+
+  const centerForMap =
+    isObjectEmpty(selectedCountry) && isObjectEmpty(selectedCity)
+      ? { lat: deviceLocation.latitude, lng: deviceLocation.longitude }
+      : !isObjectEmpty(selectedCountry) && isObjectEmpty(selectedCity)
+      ? { lat: +selectedCountry.latitude, lng: +selectedCountry.longitude }
+      : !isObjectEmpty(selectedCountry) && !isObjectEmpty(selectedCity)
+      ? { lat: +selectedCity.latitude, lng: +selectedCity.longitude }
+      : isObjectEmpty(selectedCountry) && !isObjectEmpty(selectedCity)
+      ? { lat: +selectedCity.latitude, lng: +selectedCity.longitude }
+      : { lat: defaultCoordinates.latitude, lng: defaultCoordinates.longitude };
+
+  const zoomForMap =
+    isObjectEmpty(selectedCountry) && isObjectEmpty(selectedCity)
+      ? 16
+      : !isObjectEmpty(selectedCountry) && isObjectEmpty(selectedCity)
+      ? 7
+      : 16;
 
   return (
     <Fragment>
@@ -49,6 +154,7 @@ const Landing: React.FC = () => {
             active={isChooseByCountrySection}
             onClick={handleSectionClick}
           />
+          <Button onClick={handleOnGeolocationClick} withIcon="🧭" />
           <Button
             id="1"
             title="Search by city"
@@ -56,25 +162,53 @@ const Landing: React.FC = () => {
             onClick={handleSectionClick}
           />
         </S.ButtonWrapper>
-        {Object.keys(selectedValue).length === 0 ? (
+
+        <S.MainSectionWrapper>
           <S.TableWrapper>
             <Table
-              data={isChooseByCountrySection ? countries : undefined}
+              data={isChooseByCountrySection ? countries : cities}
               title={isChooseByCountrySection ? 'Countries' : 'Cities'}
-              onCellClick={isChooseByCountrySection ? handleOnCountryPress : handleOnCityPress}
-              searchInputValue={searchInputValue}
-              onSearchInputChange={handleSearchInputChange}
-              selectedValue={selectedValue}
-              withGoogleAutoComplete={!isChooseByCountrySection}
+              onCellClick={(value) => {
+                handleOnCellClick(value, isChooseByCountrySection ? 'country' : 'city');
+              }}
+              searchInputValue={primaryInputValue}
+              onSearchInputChange={(value: string) => {
+                if (secondaryInputValue !== '') setSecondaryInputValue('');
+                setPrimaryInputValue(value);
+              }}
+              selectedValue={isChooseByCountrySection ? selectedCountry : selectedCity}
             />
+            {isChooseByCountrySection && !isObjectEmpty(selectedCountry) && (
+              <Table
+                data={cities}
+                title={`${selectedCountry?.name}'s cities`}
+                onCellClick={(value) => {
+                  handleOnCellClick(value, 'city');
+                }}
+                searchInputValue={secondaryInputValue}
+                onSearchInputChange={(value: string) => setSecondaryInputValue(value)}
+                selectedValue={selectedCity}
+              />
+            )}
           </S.TableWrapper>
-        ) : (
-          <h3>{`You have selected ${
-            selectedValue?.name
-              ? `${selectedValue.name} as a country of interest.`
-              : `${selectedValue.formatted_address} as a city of interest.`
-          }`}</h3>
-        )}
+          <S.MapWrapper>
+            <GoogleMap
+              mapContainerStyle={mapContainerStyle}
+              zoom={zoomForMap}
+              center={centerForMap}
+            >
+              {!isObjectEmpty(weather) && (
+                <InfoWindow position={centerForMap}>
+                  <Popup
+                    weather={weather}
+                    onButtonClick={handleAddRemoveCityFromFavorites}
+                    buttonTitle="Add to favourites"
+                  />
+                </InfoWindow>
+              )}
+            </GoogleMap>
+          </S.MapWrapper>
+        </S.MainSectionWrapper>
       </S.Container>
       <Footer />
     </Fragment>
--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -2,10 +2,11 @@ import styled from 'styled-components';
 
 const Container = styled.main`
   flex: 1;
-  h3 {
+  h4 {
     color: black;
-
     text-align: center;
+    margin: 0 5%;
+    font-weight: 400;
   }
 `;
 
@@ -13,6 +14,7 @@ const ButtonWrapper = styled.div`
   display: flex;
   justify-content: center;
   margin: 0.75rem;
+  align-items: center;
   button {
     margin: 0.3rem;
   }
@@ -20,7 +22,41 @@ const ButtonWrapper = styled.div`
 
 const TableWrapper = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  margin-left: 16px;
+  margin-right: 16px;
+  justify-content: space-between;
+  @media (max-width: 1026px) {
+    flex-direction: row;
+    width: 70vw;
+    margin-bottom: 16px;
+  }
+  @media (max-width: 761px) {
+    flex-direction: column;
+    align-items: center;
+    section {
+      &:first-child {
+        margin-bottom: 16px;
+      }
+    }
+  }
 `;
 
-export { Container, ButtonWrapper, TableWrapper };
+const MainSectionWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
+
+const MapWrapper = styled.div`
+  margin-right: 16px;
+  @media (max-width: 1026px) {
+    margin-right: 0;
+    margin-bottom: 16px;
+  }
+  @media (max-width: 761px) {
+    margin-top: 16px;
+  }
+`;
+
+export { Container, ButtonWrapper, TableWrapper, MainSectionWrapper, MapWrapper };
