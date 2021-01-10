67ae5b5b10a00d6a6e83faae76721d3ca5a5e5ce
Martynas Stumbra <50275578+marstu2@users.noreply.github.com>
2021-01-10 18:44:51 +0200

    styles: adjust header styles

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -4,6 +4,9 @@ const Container = styled.header`
   background-color: ${({ theme }) => theme.colors.primary};
   padding: 12px;
   text-align: center;
+  h3 {
+    font-weight: 500;
+  }
   p {
     opacity: 0.3;
   }
