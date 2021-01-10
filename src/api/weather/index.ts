884ea1010503e519f1e0c9b34ed2028e424ef818
Martynas Stumbra <50275578+marstu2@users.noreply.github.com>
2021-01-10 21:16:58 +0200

    hot-fix: added secured to http for weather api

--- src/api/weather/index.ts
+++ src/api/weather/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const WEATHER_API_KEY = '8fd7d294b0ff55fc0754e9b221f3a560';
-const HTTP_BASE = 'http://api.openweathermap.org/data/2.5/weather?units=metric&';
+const HTTP_BASE = 'https://api.openweathermap.org/data/2.5/weather?units=metric&';
 
 export const getWeatherByCoordinates = (lat: number, lng: number) => {
   return axios
